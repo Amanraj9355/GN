c64706b9f6d6c3dfbef91ff0edaf050450f222b0
Aman <rajaman9355@gmail.com>
2025-07-31 11:51:53 +0530

    Added Fix

--- Frontend/src/context/store.jsx
+++ Frontend/src/context/store.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-  const backendUrl = "http://localhost:8000";
+  const backendUrl = "https://gn-production-fdc6.up.railway.app";
   const [isAuthVisible, setIsAuthVisible] = useState(false);
   const [token, setToken] = useState(localStorage.getItem("token") || "");
   const [cartItems, setCartItems] = useState({});
